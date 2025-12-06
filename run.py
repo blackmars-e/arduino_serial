ec7da62504b53d507224696eac960a77a12d355d
blackmars-e <78505821+blackmars-e@users.noreply.github.com>
2025-12-06 10:28:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -10,18 +10,16 @@ def send(cmd):
         ser.write((cmd+"\n").encode())
         time.sleep(0.2)
 
-# Simple TCP server listens on 7070
+# TCP server auf Port 7070
 sock = socket.socket()
-sock.bind(("0.0.0.0",7070))
+sock.bind(("0.0.0.0", 7070))
 sock.listen(5)
 print("Arduino Serial Add-on ready")
 
 while True:
-    c,_ = sock.accept()
-    data = c.recv(64).decode().strip()
+    conn, addr = sock.accept()
+    data = conn.recv(64).decode().strip()
     if data:
-        print("Sending:",data)
+        print("Sending:", data)
         send(data)
-    c.close()
-
-
+    conn.close()
