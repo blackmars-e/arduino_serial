4db90fc3bdeb8a83dc2f2774ffbb5b4d75223692
blackmars-e <78505821+blackmars-e@users.noreply.github.com>
2025-12-17 07:29:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
-echo "--- Starte Arduino Serial Controller ---"
+echo "--- Starte Arduino Serial Controller Add-on ---"
+echo "Prüfe Hardware..."
+if [ -c "/dev/ttyUSB0" ]; then
+    echo "✅ /dev/ttyUSB0 gefunden."
+else
+    echo "⚠️ /dev/ttyUSB0 nicht gefunden! Das Skript wird versuchen, sich zu verbinden, sobald es verfügbar ist."
+fi
 
-# Zeige Pfade zur Fehlersuche im Log
-echo "Python Version:"
-python3 --version
-echo "Installierte Pakete:"
-pip3 list | grep pyserial
-
-# Starte das Skript
+# Starte Python Skript
 exec python3 -u /app/run.py
