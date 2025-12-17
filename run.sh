6886200c84854702eb64ea64c1fff10026426d47
blackmars-e <78505821+blackmars-e@users.noreply.github.com>
2025-12-17 06:59:08 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-
 echo "Starte Arduino Serial Controller..."
-
-# Starte Python im Unbuffered Mode (-u), damit Logs sofort sichtbar sind
-# exec ersetzt den aktuellen Shell-Prozess durch Python (wichtig für Signale)
-exec /opt/venv/bin/python3 -u /app/run.py
+exec python3 -u /app/run.py
