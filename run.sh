70da61f871c9b436ebb24e7d56156cc2b52615ee
blackmars-e <78505821+blackmars-e@users.noreply.github.com>
2025-12-17 07:20:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env sh
 set -e
-echo "Starte Arduino Serial Controller..."
+
+echo "--- Starte Arduino Serial Controller ---"
+
+# Zeige Pfade zur Fehlersuche im Log
+echo "Python Version:"
+python3 --version
+echo "Installierte Pakete:"
+pip3 list | grep pyserial
+
+# Starte das Skript
 exec python3 -u /app/run.py
